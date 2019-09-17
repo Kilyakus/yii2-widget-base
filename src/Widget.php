17bce715f967ec5880e07951be0ac758fecc0524
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-09-17 05:03:30 +0500

    initial commit

--- src/Widget.php
+++ src/Widget.php
@@ -1,36 +1,31 @@
 <?php
-
-/**
- * @package   yii2-krajee-base
- * @author    Kartik Visweswaran <kartikv2@gmail.com>
- * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2019
- * @version   2.0.5
- */
-
 namespace kilyakus\widgets;
 
 use yii\base\InvalidConfigException;
 use yii\base\Widget as YiiWidget;
 
-/**
- * Base class for widgets extending [[YiiWidget]] used in Krajee extensions.
- *
- * @author Kartik Visweswaran <kartikv2@gmail.com>
- */
 class Widget extends YiiWidget implements BootstrapInterface
 {
     use TranslationTrait;
     use WidgetTrait;
 
-    /**
-     * @var array HTML attributes or other settings for widgets.
-     */
     public $options = [];
-
+    
     /**
-     * @inheritdoc
-     * @throws InvalidConfigException
+     * @var array the options for the underlying Engine JS plugin.
+     * Please refer to the corresponding Engine plugin Web page for possible options.
+     * For example, [this page](http://yii2engine.icron.org/javascript.html#portlet) shows
+     * how to use the "Portlet" plugin and the supported options (e.g. "loadSuccess").
      */
+    public $clientOptions = [];
+    /**
+     * @var array the event handlers for the underlying Engine JS plugin.
+     * Please refer to the corresponding Engine plugin Web page for possible events.
+     * For example, [this page](http://yii2engine.icron.org/javascript.html#portlet) shows
+     * how to use the "Portlet" plugin and the supported events (e.g. "close.mr.portlet").
+     */
+    public $clientEvents = [];
+
     public function init()
     {
         $this->initBsVersion();
@@ -40,5 +35,27 @@ class Widget extends YiiWidget implements BootstrapInterface
             $this->options['id'] = $this->getId();
         }
         $this->initDestroyJs();
+    }
+
+    /**
+     для yii2-widget-portlet. Нужно проверить, удалить если появятся ошибки в других плагинах
+     */
+
+    protected function registerPlugin($name)
+    {
+        $view = $this->getView();
+        $id = $this->options['id'];
+        if ($this->clientOptions !== false) {
+            $options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
+            $js = "jQuery('#$id').$name($options);";
+            $view->registerJs($js);
+        }
+        if (!empty($this->clientEvents)) {
+            $js = [];
+            foreach ($this->clientEvents as $event => $handler) {
+                $js[] = "jQuery('#$id').on('$event', $handler);";
+            }
+            $view->registerJs(implode("\n", $js));
+        }
     }
 }
