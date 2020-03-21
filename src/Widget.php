3878e2d7f09f02e67a1d9d31fa190e490c33d1fd
Kilyakus <aleksandr.kilyakov@gmail.com>
2020-03-21 13:23:39 +0500

    initial commit

--- src/Widget.php
+++ src/Widget.php
@@ -11,20 +11,9 @@ class Widget extends YiiWidget implements BootstrapInterface
     use WidgetTrait;
 
     public $options = [];
-    
-    /**
-     * @var array the options for the underlying Engine JS plugin.
-     * Please refer to the corresponding Engine plugin Web page for possible options.
-     * For example, [this page](http://yii2engine.icron.org/javascript.html#portlet) shows
-     * how to use the "Portlet" plugin and the supported options (e.g. "loadSuccess").
-     */
+
     public $clientOptions = [];
-    /**
-     * @var array the event handlers for the underlying Engine JS plugin.
-     * Please refer to the corresponding Engine plugin Web page for possible events.
-     * For example, [this page](http://yii2engine.icron.org/javascript.html#portlet) shows
-     * how to use the "Portlet" plugin and the supported events (e.g. "close.mr.portlet").
-     */
+
     public $clientEvents = [];
 
     public function init()
@@ -42,21 +31,21 @@ class Widget extends YiiWidget implements BootstrapInterface
      для yii2-widget-portlet. Нужно проверить, удалить если появятся ошибки в других плагинах
      */
 
-    protected function registerPlugin($name)
-    {
-        $view = $this->getView();
-        $id = $this->options['id'];
-        if ($this->clientOptions !== false) {
-            $options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
-            $js = "jQuery('#$id').$name($options);";
-            $view->registerJs($js);
-        }
-        if (!empty($this->clientEvents)) {
-            $js = [];
-            foreach ($this->clientEvents as $event => $handler) {
-                $js[] = "jQuery('#$id').on('$event', $handler);";
-            }
-            $view->registerJs(implode("\n", $js));
-        }
-    }
+    // public function registerPlugin($name)
+    // {
+    //     $view = $this->getView();
+    //     $id = $this->options['id'];
+    //     if ($this->clientOptions !== false) {
+    //         $options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
+    //         $js = "jQuery('#$id').$name($options);";
+    //         $view->registerJs($js);
+    //     }
+    //     if (!empty($this->clientEvents)) {
+    //         $js = [];
+    //         foreach ($this->clientEvents as $event => $handler) {
+    //             $js[] = "jQuery('#$id').on('$event', $handler);";
+    //         }
+    //         $view->registerJs(implode("\n", $js));
+    //     }
+    // }
 }
